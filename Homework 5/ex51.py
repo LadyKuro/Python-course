bb1a9bb50aa02c8344eccc3d7e40805d67b02607
Alicja Kawala <alicja.kawala@gmail.com>
2024-04-04 16:13:38 +0200

    Corrected exercise 5.1

--- Homework 5/ex51.py
+++ Homework 5/ex51.py
@@ -3,8 +3,10 @@ import os
 def find_pdf_size(directory):
     """Find the size of all PDF files in the given directory and its subdirectories."""
     pdf_list = []
+    size = 0
     for root, dirs, files in os.walk(directory):
         pdf_list.extend([name for name in files if name.endswith('.pdf')])
-    print(sum(os.path.getsize(os.path.join(root, file)) for file in pdf_list))
+        size+=sum(os.path.getsize(os.path.join(root, file)) for file in pdf_list)
+    print(size)
 
 find_pdf_size(".")
